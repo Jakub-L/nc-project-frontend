667eac3ffd23026982161567c835d8117cefaa7b
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-07 10:20:48 +0100

    Add marker display

--- src/components/PinMap.jsx
+++ src/components/PinMap.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Map, TileLayer } from 'react-leaflet';
+import {
+  Map, Marker, Popup, TileLayer,
+} from 'react-leaflet';
 import '../styles/PinMap.css';
+import sampleLocations from '../sample-data/locations.json';
 
 class PinMap extends Component {
   state = {
@@ -9,20 +11,34 @@ class PinMap extends Component {
     zoom: 16,
     attributionText: '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     tileLayerUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    locations: sampleLocations,
   };
 
   render() {
-    const { center, zoom, attributionText, tileLayerUrl } = this.state;
+    const {
+      center, zoom, attributionText, tileLayerUrl, locations,
+    } = this.state;
     return (
       <div className="PinMap">
         <Map id="map-container" center={center} zoom={zoom}>
           <TileLayer attribution={attributionText} url={tileLayerUrl} />
+          {locations.map((location) => {
+            const {
+              longitude, latitude, note, photo_url, pin_id,
+            } = location;
+            return (
+              <Marker key={pin_id} position={[latitude, longitude]}>
+                <Popup>
+                  <img src={photo_url} alt="Pin" />
+                  {note}
+                </Popup>
+              </Marker>
+            );
+          })}
         </Map>
       </div>
     );
   }
 }
-
-PinMap.propTypes = {};
 
 export default PinMap;
