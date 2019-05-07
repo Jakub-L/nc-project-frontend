c402a5e9e5414c4bb0f0b273f4ea35b40dc48c69
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-07 09:44:11 +0100

    Change map to stateful component

--- src/components/PinMap.jsx
+++ src/components/PinMap.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Map, TileLayer } from 'react-leaflet';
 import '../styles/PinMap.css';
 
-const PinMap = props => (
-  <div className="PinMap">
-    <Map id="map-container" center={[53.795, -1.55]} zoom={16}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        // url="http://a.tile.stamen.com/toner/{z}/{x}/{y}.png"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </Map>
-  </div>
-);
+class PinMap extends Component {
+  state = {
+    center: [53.795, -1.55],
+    zoom: 16,
+    attributionText: '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    tileLayerUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  };
+
+  render() {
+    const { center, zoom, attributionText, tileLayerUrl } = this.state;
+    return (
+      <div className="PinMap">
+        <Map id="map-container" center={center} zoom={zoom}>
+          <TileLayer attribution={attributionText} url={tileLayerUrl} />
+        </Map>
+      </div>
+    );
+  }
+}
 
 PinMap.propTypes = {};
 
